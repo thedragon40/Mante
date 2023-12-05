2ac7a017f5294d34095f660e3a00f10c3f3c67e9
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-06 00:41:00 +0100

    Update Interfaz.ps1

--- Interfaz.ps1
+++ Interfaz.ps1
@@ -1,18 +1,23 @@
 # Interfaz.ps1
 
-# Cargar la Asamblea para Windows Forms
 Add-Type -AssemblyName System.Windows.Forms
 
-# Función para crear y mostrar la interfaz gráfica
 function Mostrar-Interfaz {
     $form = New-Object Windows.Forms.Form
     $form.Text = "Mantenimiento de PC"
     $form.StartPosition = "CenterScreen"
+    $form.Size = New-Object Drawing.Size(400, 300)
 
-    $estadosFunciones = @{}
-    $topMargin = 10
+    $outputTextBox = New-Object Windows.Forms.TextBox
+    $outputTextBox.Multiline = $true
+    $outputTextBox.ScrollBars = "Vertical"
+    $outputTextBox.Location = New-Object Drawing.Point(10, 10)
+    $outputTextBox.Size = New-Object Drawing.Size(380, 150)
+    $outputTextBox.ReadOnly = $true
+    $form.Controls.Add($outputTextBox)
 
-    $opcionesMenu = @(
+    $checkBoxes = @{}
+    $options = @(
         "Desfragmentar el disco",
         "Limpiar archivos temporales",
         "Verificar y reparar errores en el disco",
@@ -22,62 +27,47 @@ function Mostrar-Interfaz {
         "Respaldar archivos importantes",
         "Crear un punto de restauración del sistema",
         "Desinstalar aplicaciones no deseadas",
-        "Gestionar programas de inicio del sistema",
-        "Ejecutar todas las opciones",
-        "Salir"
+        "Gestionar programas de inicio del sistema"
     )
 
-    foreach ($opcion in $opcionesMenu) {
+    $topMargin = 170
+
+    foreach ($option in $options) {
         $checkBox = New-Object Windows.Forms.CheckBox
-        $checkBox.Text = $opcion
+        $checkBox.Text = $option
         $checkBox.Location = New-Object Drawing.Point(10, $topMargin)
         $checkBox.AutoSize = $true
         $form.Controls.Add($checkBox)
-
-        $checkBox.Add_Click({
-            $checkBox.Text  # Esta línea te dará el texto de la opción seleccionada
-            # Puedes agregar código adicional aquí si es necesario
-        })
-
-        $estadosFunciones[$opcion] = $checkBox
+        $checkBoxes[$option] = $checkBox
         $topMargin += $checkBox.Height + 5
     }
 
     $buttonEjecutar = New-Object Windows.Forms.Button
-    $buttonEjecutar.Location = New-Object Drawing.Point(150, $topMargin)
-    $buttonEjecutar.Size = New-Object Drawing.Size(200, 30)
+    $buttonEjecutar.Location = New-Object Drawing.Point(10, $topMargin)
+    $buttonEjecutar.Size = New-Object Drawing.Size(150, 30)
     $buttonEjecutar.Text = "Ejecutar"
     $buttonEjecutar.Add_Click({
-        $funcionesActivadas = $estadosFunciones.Keys | Where-Object { $estadosFunciones[$_].Checked }
+        $outputTextBox.Clear()
+        $selectedOptions = $checkBoxes.Values | Where-Object { $_.Checked }
 
-        if ($funcionesActivadas.Count -eq 0) {
-            Write-Host "No se ha seleccionado ninguna función para ejecutar."
+        if ($selectedOptions.Count -eq 0) {
+            $outputTextBox.AppendText("No se ha seleccionado ninguna función para ejecutar.")
             return
         }
 
-        # Desactivar otros controles y desmarcar funciones
-        foreach ($control in $estadosFunciones.Values) {
-            $control.Enabled = $false
-            $control.Checked = $false
+        foreach ($option in $selectedOptions) {
+            $outputTextBox.AppendText("Ejecutando: $($option.Text)`r`n")
+            Ejecutar-Funcion $option.Text
         }
-
-        foreach ($funcion in $funcionesActivadas) {
-            Write-Host "Ejecutando: $funcion"
-            Ejecutar-Funcion $funcion
-        }
-
-        # Cerrar la ventana después de ejecutar
-        $form.Close()
     })
     $form.Controls.Add($buttonEjecutar)
 
     $buttonSalir = New-Object Windows.Forms.Button
-    $buttonSalir.Location = New-Object Drawing.Point(150, $topMargin + 40)
-    $buttonSalir.Size = New-Object Drawing.Size(200, 30)
+    $buttonSalir.Location = New-Object Drawing.Point(180, $topMargin)
+    $buttonSalir.Size = New-Object Drawing.Size(150, 30)
     $buttonSalir.Text = "Salir"
     $buttonSalir.Add_Click({
         $form.Close()
-        # Exit-PSSession  # Este comando no es necesario aquí
     })
     $form.Controls.Add($buttonSalir)
 
@@ -86,23 +76,11 @@ function Mostrar-Interfaz {
 }
 
 function Ejecutar-Funcion($funcion) {
+    # Implementa la lógica de las funciones según tus necesidades
     switch ($funcion) {
-        "Desfragmentar el disco" { Desfragmentar-Disco }
-        "Limpiar archivos temporales" { Limpiar-Temporales }
-        "Verificar y reparar errores en el disco" { Verificar-Errores-Disco }
-        "Actualizar el sistema" { Actualizar-Sistema }
-        "Escanear seguridad con Windows Defender" { Escanear-Seguridad }
-        "Liberar espacio en disco" { Liberar-Espacio }
-        "Respaldar archivos importantes" { Respaldar-Archivos }
-        "Crear un punto de restauración del sistema" { Crear-Punto-Restauracion }
-        "Desinstalar aplicaciones no deseadas" { Desinstalar-Aplicaciones }
-        "Gestionar programas de inicio del sistema" { Gestionar-Inicio-Sistema }
-        "Ejecutar todas las opciones" {
-            foreach ($opcion in $opcionesMenu) {
-                Ejecutar-Funcion $opcion
-            }
-        }
-        "Salir" { $form.Close() }
+        "Desfragmentar el disco" { Write-Host "Desfragmentando el disco" }
+        "Limpiar archivos temporales" { Write-Host "Limpiando archivos temporales" }
+        # Agrega más funciones según sea necesario
         default { Write-Host "Opción no válida: $funcion" }
     }
 }
