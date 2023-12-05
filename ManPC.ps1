ee21695a2a5d8232f9dfd1bc980b6e30747e78ea
JuanQ <87898162+thedragon40@users.noreply.github.com>
2023-12-05 18:47:51 +0100

    Update ManPC.ps1

--- ManPC.ps1
+++ ManPC.ps1
@@ -7,7 +7,7 @@ $OutputEncoding = [System.Text.Encoding]::UTF8
 iex (iwr -useb https://raw.githubusercontent.com/thedragon40/Mante/main/Funciones.ps1).Content
 
 # Cargar interfaz desde GitHub
-iex (iwr -useb https://raw.githubusercontent.com/thedragon40/Mante/main/Interfaz1.ps1).Content
+iex (iwr -useb https://raw.githubusercontent.com/thedragon40/Mante/main/Interfaz.ps1).Content
 
 # Ejecución del programa
 Mostrar-Interfaz
